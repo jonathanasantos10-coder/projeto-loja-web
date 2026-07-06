f1fbfc2b91c0fc7a566e0f2d101c0e020660b13b
Jonathan <jonathan.a.santos10@aluno.senai.br>
2026-07-06 19:51:27 -0300

    corrigido novamente o erro de cor no footer, pois o fix que eu tinha feito fez com que os dados de valores no carrinho ficassem ilegiveis

--- js/carrega_produtos.js
+++ js/carrega_produtos.js
@@ -1,3 +1,31 @@
 import { produtos } from "./produtos.js";
 
 
+const section_card = document.querySelector(#cards)
+
+
+const listarProdutos = ()=>{
+    section_cards.innerHTML = ''
+    produtos.forEach((elem, i)=>{
+
+        const divCard = document.querySelector('div')
+
+        divCard.setAttribute('class','card')
+        
+        const imgProduto = document.createElement('img')
+        imgProduto.setAttribute('src',elem.caminho_da_imagem)
+        imgProduto.setAttribute('alt', elem.descricao_produto)
+        imgProduto.setAttribute('class', 'img_card')
+
+        const h2Titulo = document.createElement('h2')
+        h2Titulo.innerHTML = elem.descricao_produto
+
+        const divValor = document.createElement('div')
+        divValor.setAttribute('class', 'valor_card')
+        divValor.innerHTML = `R$ ${parseFloat(elem.valor_unitario).toFixed(2).replace('.',',')}`
+
+        const btnCard = document.createElement('button')
+        btnCard.setAttribute('class', 'btn_card')
+        btnCard.innerHTML = 'Adicionar'
+    })
+}
