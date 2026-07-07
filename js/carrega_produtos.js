8df7861df8f7fc85eb45042e421c7ba8f03383d4
Jonathan <jonathan.a.santos10@aluno.senai.br>
2026-07-06 21:22:21 -0300

    .

--- js/carrega_produtos.js
+++ js/carrega_produtos.js
@@ -20,12 +20,21 @@ const listarProdutos = ()=>{
         const h2Titulo = document.createElement('h2')
         h2Titulo.innerHTML = elem.descricao_produto
 
-        const divValor = document.createElement('div')
+        const h3Valor = document.createElement('div')
         divValor.setAttribute('class', 'valor_card')
         divValor.innerHTML = `R$ ${parseFloat(elem.valor_unitario).toFixed(2).replace('.',',')}`
 
         const btnCard = document.createElement('button')
         btnCard.setAttribute('class', 'btn_card')
         btnCard.innerHTML = 'Adicionar'
+
+        divCard.appendChild(imgProduto)
+        divCard.appendChild(h2Titulo)
+        divCard.appendChild(h3Valor)
+        divCard.appendChild(btnCard)
+
+        section_cards.appendChild(divCard)
     })
 }
+
+listarProdutos()
