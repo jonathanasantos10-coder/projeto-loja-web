88b5c7d954445a64c37191a3b41d1354db72e20c
jonathan <jonathan.a.santos10@aluno.senai.br>
2026-07-16 20:26:07 -0300

    JS: Alterado o arquivo monta_tela_Carrinho js para fazer com que ele interagisse com a lógica if na função additem do arquivo carrinho.js, adicionado três funções novas (adicionar quantidade, remover quantidade e atualizar total). CSS: Arrumado CSS para as outras páginas fazendo com que elas ficassem quase padrão.

--- js/monta_tela_carrinho.js
+++ js/monta_tela_carrinho.js
@@ -13,12 +13,38 @@ const montaTelaCarrinho = () => {
         sectionItem.innerHTML = `<img src='${elem.caminho_da_imagem}' alt=${elem.descricao_produto}/> 
         <p class='descricao'>${elem.descricao_produto}</p> 
         <p class='vlr-unitario'>${elem.valor_unitario}</p> 
-        <input type="number" name="quant" id="quant" class="input-item" value=${1}> 
-        <p class="tot-item">${elem.valor_unitario * 2}</p>`
+        <p class='quantidade'> Quantidade:${elem.quantidade}</p>
+        <button type='button' id='btn_mais' class='btn_mais'>+</button>
+        <button type='button' id='btn_menos' class='btn_menos'>-</button>
+        <p class="tot-item">${elem.valor_unitario * elem.quantidade}</p>`
+
+
 
         sectionItensCarrinho.appendChild(sectionItem)
 
+        // pegando elemento do "dom" dinamico criado no section item (o botoa de add)
+        const btn_Mais = sectionItem.querySelector('#btn_mais')
+        // arrow function do botao de add
+        btn_Mais.addEventListener('click', () => {
+            sectionItem.querySelector('.quantidade').innerHTML = `Quantidade:${++elem.quantidade}` // para pegar uma classe é igual css, basta usar .
+            atualizaTotal()
+        })
+        // arrow fuction do botao de remover 
+        const btn_Menos = sectionItem.querySelector('#btn_menos')
+        btn_Menos.addEventListener('click', () => {
+            sectionItem.querySelector('.quantidade').innerHTML = `Quantidade:${elem.quantidade <= 1 ? 1 : --elem.quantidade}`
+            atualizaTotal()
+        })
+        
+        // pegando o elemento do dom dinamico criado no section item, desta vez o paragrafo de total
+        const total = sectionItem.querySelector('.tot-item')
+        // função que atualiza o total baseado na quantidade e valor unitario do item
+        function atualizaTotal() {
+            total.innerHTML = elem.valor_unitario * elem.quantidade
+        }
+
         const imgRemover = document.createElement('img')
+        imgRemover.setAttribute('class', 'img_del')
         imgRemover.setAttribute('src', '../images/icons/trash.png')
         imgRemover.setAttribute('alt', 'img-remover')
 
@@ -37,7 +63,9 @@ const montaTelaCarrinho = () => {
 
 
 
-const removerItemCarrinho = (pos)=>{
+
+
+const removerItemCarrinho = (pos) => {
     removeItem(pos)
 
     montaTelaCarrinho()
